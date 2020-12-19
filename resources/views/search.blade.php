5f87faf619ea71b73be51ecd2db753d9aa0668b5
Pradip <pradipbasel1619@gmail.com>
2020-12-19 21:09:10 +0545

    login and logout done

---
+++ resources/views/search.blade.php
@@ -0,0 +1,24 @@
+@extends('master')
+@section('content')
+<div class="custom-product">
+<div class="col-sm-4">
+    <a href="#">Filter</a>
+</div>
+<div class="col-sm-4">
+<div class="trending-wrapper">
+  <h4>Availabe Products Result</h4>
+  @foreach ($products as $item)
+  <div class="searched-item">
+    <a href="detail/{{$item['id']}}">
+      <img class="trending-img" src="{{$item['gallery']}}">
+      <div class=" ">
+        <h3>{{$item['name']}}</h3>
+        <h3>{{$item['description']}}</h3>
+      </div>
+      </a>
+    </div>
+  @endforeach
+  </div>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +16,14 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('/login');
 });
 
+Route::view("/login","login");
 Route::post("/login",[UserController::class,'login']);
 Route::get("/",[ProductController::class,'index']);
+Route::get("detail/{id}",[ProductController::class,'detail']);
+Route::get("search",[ProductController::class,'search']);
+Route::post("add_to_cart",[ProductController::class,'addToCart']);
